76e173572a4ee8c8325de39b9abc99fedd20cf89
ASRAF2306 <99631625+ASRAF2306@users.noreply.github.com>
2022-04-07 20:06:27 +0700

    Add files via upload

---
+++ day48_java_method_overidding.java
@@ -0,0 +1,37 @@
+
+package asraf;
+
+import java.util.*;
+import java.io.*;
+
+public class day48_java_method_overidding {
+    
+}
+
+
+class BiCycle{
+	String define_me(){
+		return "a vehicle with pedals.";
+	}
+}
+
+class MotorCycle extends BiCycle{
+	String define_me(){
+		return "a cycle with an engine.";
+	}
+	
+	MotorCycle(){
+		System.out.println("Hello I am a motorcycle, I am "+ define_me());
+
+		//String temp=define_me(); //Fix this line
+        String temp= super.define_me();
+
+		System.out.println("My ancestor is a cycle who is "+ temp );
+	}
+	
+}
+class Solution{
+	public static void main(String []args){
+		MotorCycle M=new MotorCycle();
+	}
+}
